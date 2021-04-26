b96a987643ed8e7355c1e27706636fe22aba76b7
86233 <1341521266@qq.com>
2021-04-26 20:47:28 +0800

    单个usb相机采图显示

--- main_cv.py
+++ main_cv.py
@@ -1,17 +1,42 @@
 import cv2
+import sys
 from cv_show import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
 
 class MyMainWindow(QMainWindow , Ui_MainWindow):
     def __init__(self ):
         super(MyMainWindow, self).__init__()
         self.setupUi(self)
         self.cam_time = QtCore.QTimer()
-        self.open_cam.clicked.connect()
-        self.close_cam.connect()
+        self.cam_time.timeout.connect(self.show_pic)
+        self.open_cam.clicked.connect(self.start_grap)
+        self.close_cam.clicked.connect(self.stop_cam)
 
     def start_grap(self):
-        self.cap = cv2.VideoCapture()
-        self.CAM_NUM = 0
+        self.cap = cv2.VideoCapture(0)
+        self.cam_time.start(100)
 
+    def stop_cam(self):
+        self.cap.release()
+        self.cam_time.stop()
+
+    def show_pic(self):
+        ret, img = self.cap.read()
+        if not ret:
+            print('read error!\n')
+            return
+        cv2.flip(img, 1, img)
+        cur_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        heigt, width = cur_frame.shape[:2]
+        pixmap = QImage(cur_frame, width, heigt, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(pixmap)
+        self.label.setPixmap(pixmap)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MyMainWindow()
+    win.show()
+    sys.exit(app.exec_())
