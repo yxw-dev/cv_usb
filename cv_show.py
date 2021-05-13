23a3705a52ac531708b05b9546d5e5a87ea5ade3
86233 <1341521266@qq.com>
2021-05-13 08:39:28 +0800

    双usb相机采集

--- cv_show.py
+++ cv_show.py
@@ -11,7 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(1509, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.open_cam = QtWidgets.QPushButton(self.centralwidget)
@@ -23,9 +23,15 @@ class Ui_MainWindow(object):
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(200, 37, 561, 481))
         self.label.setObjectName("label")
+        self.open_cam_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.open_cam_2.setGeometry(QtCore.QRect(30, 80, 112, 34))
+        self.open_cam_2.setObjectName("open_cam_2")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(730, 20, 561, 481))
+        self.label_2.setObjectName("label_2")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 30))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1509, 30))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -38,7 +44,9 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.open_cam.setText(_translate("MainWindow", "打开相机"))
+        self.open_cam.setText(_translate("MainWindow", "打开相机1"))
         self.close_cam.setText(_translate("MainWindow", "关闭相机"))
         self.label.setText(_translate("MainWindow", "TextLabel"))
+        self.open_cam_2.setText(_translate("MainWindow", "打开相机2"))
+        self.label_2.setText(_translate("MainWindow", "TextLabel"))
 
--- main_cv.py
+++ main_cv.py
@@ -10,20 +10,28 @@ class MyMainWindow(QMainWindow , Ui_MainWindow):
         super(MyMainWindow, self).__init__()
         self.setupUi(self)
         self.cam_time = QtCore.QTimer()
+        self.cam_time2 = QtCore.QTimer()
         self.cam_time.timeout.connect(self.show_pic)
+        self.cam_time2.timeout.connect(self.show_pic2)
         self.open_cam.clicked.connect(self.start_grap)
+        self.open_cam_2.clicked.connect(self.start_grap2)
         self.close_cam.clicked.connect(self.stop_cam)
 
+
     def start_grap(self):
-        self.cap = cv2.VideoCapture(0)
+        self.cap1 = cv2.VideoCapture(2+ cv2.CAP_DSHOW)
         self.cam_time.start(100)
 
+    def start_grap2(self):
+        self.cap2 = cv2.VideoCapture(1 + cv2.CAP_DSHOW)
+        self.cam_time2.start(100)
+
     def stop_cam(self):
-        self.cap.release()
+        self.cap1.release()
         self.cam_time.stop()
 
     def show_pic(self):
-        ret, img = self.cap.read()
+        ret, img = self.cap1.read()
         if not ret:
             print('read error!\n')
             return
@@ -34,6 +42,17 @@ class MyMainWindow(QMainWindow , Ui_MainWindow):
         pixmap = QPixmap.fromImage(pixmap)
         self.label.setPixmap(pixmap)
 
+    def show_pic2(self):
+        ret_2, img_2 = self.cap2.read()
+        if not ret_2:
+            print('read error!\n')
+            return
+        cv2.flip(img_2, 1, img_2)
+        cur_frame = cv2.cvtColor(img_2, cv2.COLOR_BGR2RGB)
+        heigt, width = cur_frame.shape[:2]
+        pixmap = QImage(cur_frame, width, heigt, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(pixmap)
+        self.label_2.setPixmap(pixmap)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
