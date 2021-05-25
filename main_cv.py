bbdd1d5887338047fd8d126310bbf67b748b1fb5
86233 <1341521266@qq.com>
2021-05-25 14:28:38 +0800

    布局实现适应窗口分辨率

--- main_cv.py
+++ main_cv.py
@@ -4,6 +4,7 @@ import time
 from cv_show import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 
 class MyMainWindow(QMainWindow , Ui_MainWindow):
@@ -15,14 +16,75 @@ class MyMainWindow(QMainWindow , Ui_MainWindow):
         self.start_time2 = time.time()
         self.frame1=0
         self.frame2=0
+        self.width_step_max = 50
+        self.height_step_max = 50
+        self.width_step = 50        #横向两红线间隔默认值
+        self.height_step = 50       #纵向两红线间隔默认值
+        self.doubleSpinBox.setValue(self.height_step)
+        self.doubleSpinBox_2.setValue(self.width_step)
         self.cam_time = QtCore.QTimer()
         self.cam_time2 = QtCore.QTimer()
         self.cam_time.timeout.connect(self.show_pic)
         self.cam_time2.timeout.connect(self.show_pic2)
-        self.horizontalSlider.valueChanged.connect(self.moveline)
         self.actionExit.triggered.connect(self.close)
         self.action_1.triggered.connect(self.start_grap)
         self.action_2.triggered.connect(self.start_grap2)
+        self.setFocus()
+
+    #键盘监控事件
+    def keyPressEvent(self,event):
+        if(event.key() == Qt.Key_Q):
+            self.close()
+        if(event.key() == Qt.Key_F):
+            self.start_grap()
+        if (event.key() == Qt.Key_B):
+            self.start_grap2()
+        if(event.key() == Qt.Key_A):
+            self.horizontalSlider.setValue(self.horizontalSlider.value() - 1)
+            self.drawline()
+        if (event.key() == Qt.Key_D):
+            self.horizontalSlider.setValue(self.horizontalSlider.value() + 1)
+            self.drawline()
+        if(event.key() == Qt.Key_W):
+            if(self.width_step < self.width_step_max):
+                self.width_step = self.width_step + 1
+            else:
+                self.width_step = 0
+            self.drawline()
+            self.doubleSpinBox_2.setValue(self.width_step)
+        if (event.key() == Qt.Key_S):
+            if (self.width_step > 0):
+                self.width_step = self.width_step - 1
+            else:
+                self.width_step = self.width_step_max
+            self.drawline()
+            self.doubleSpinBox_2.setValue(self.width_step)
+
+        if (event.key() == Qt.Key_Left):
+            self.verticalSlider.setValue(self.verticalSlider.value() + 1)
+            self.drawline()
+        if (event.key() == Qt.Key_Right):
+            self.verticalSlider.setValue(self.verticalSlider.value() - 1)
+            self.drawline()
+        if (event.key() == Qt.Key_Up):
+            if (self.height_step < self.height_step_max):
+                self.height_step = self.height_step + 1
+            else:
+                self.height_step = 0
+            self.drawline()
+            self.doubleSpinBox.setValue(self.height_step)
+        if (event.key() == Qt.Key_Down):
+            if (self.height_step > 0):
+                self.height_step = self.height_step - 1
+            else:
+                self.height_step = self.height_step_max
+            self.drawline()
+            self.doubleSpinBox.setValue(self.height_step)
+
+
+
+    def drawline(self):
+        print(1)
     #移动滑块调节指示线位置
     def moveline(self):
         sta_x = self.horizontalSlider.x()
@@ -38,12 +100,14 @@ class MyMainWindow(QMainWindow , Ui_MainWindow):
         self.frame1=0
         self.cap1 = cv2.VideoCapture(1+ cv2.CAP_DSHOW)
         self.cam_time.start(60)
+        self.label_5.setText("正面相机：运行")
 
     def start_grap2(self):
         self.start_time2 = time.time()
         self.frame2=0
         self.cap2 = cv2.VideoCapture(0 + cv2.CAP_DSHOW)
         self.cam_time2.start(60)
+        self.label_7.setText("侧面相机：运行")
 
     def stop_cam(self):
         self.cap1.release()
@@ -53,6 +117,8 @@ class MyMainWindow(QMainWindow , Ui_MainWindow):
         ret, img = self.cap1.read()
         if not ret:
             print('read error!\n')
+            self.label_5.setText("正面相机：打开失败")
+            self.cam_time.stop()
             return
         #cv2.flip(img, 1, img)
         cur_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -72,6 +138,8 @@ class MyMainWindow(QMainWindow , Ui_MainWindow):
         ret_2, img_2 = self.cap2.read()
         if not ret_2:
             print('read error!\n')
+            self.label_7.setText("侧面相机：打开失败")
+            self.cam_time2.stop()
             return
         cv2.flip(img_2, 1, img_2)
         cur_frame = cv2.cvtColor(img_2, cv2.COLOR_BGR2RGB)
@@ -92,3 +160,6 @@ if __name__ == "__main__":
     win = MyMainWindow()
     win.showFullScreen()
     sys.exit(app.exec_())
+
+
+
